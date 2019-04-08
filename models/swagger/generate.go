6def6c72d8d409ee509c4dd60794bb5e6ce6db9a
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-04-08 11:52:44 -0400

    Move the documentation to a standard location

--- models/swagger/generate.go
+++ models/swagger/generate.go
@@ -3,3 +3,4 @@ package swagger
 
 //go:generate docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli generate -i /data/models/swagger/swagger_1_8_0.json -l go -o /data/pureport/swagger
 //go:generate gofmt -s -w ../../pureport/swagger
+//go:generate mv ../../pureport/swagger/docs ../../docs/swagger
