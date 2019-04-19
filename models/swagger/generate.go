5500781c27a1f95b94593d6e465e6f02bd504f70
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-04-19 11:45:34 -0400

    Fix for Instant type

--- models/swagger/generate.go
+++ models/swagger/generate.go
@@ -1,7 +1,7 @@
 // Package swagger Generator based on the swagger API definition
 package swagger
 
-//go:generate docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli generate -i /data/models/swagger/swagger_1_8_0.json -l go -o /data/pureport/swagger
+//go:generate docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli generate -i /data/models/swagger/kato_swagger.json -l go -o /data/pureport/swagger
 //go:generate gofmt -s -w ../../pureport/swagger
 //go:generate mv ../../pureport/swagger/docs ../../docs/swagger
 //go:generate mv ../../pureport/swagger/README.md ../../docs/README.md
