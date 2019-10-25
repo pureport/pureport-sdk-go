dd9a0381831793f06d5f85ece9358df4f6ba0bfe
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-10-25 12:13:47 -0400

    Update to 2.4.9 to fix Out Of Memory Error

--- models/swagger/build.sh
+++ models/swagger/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli:2.4.5 generate -i /data/models/swagger/kato_swagger.json -l go -o /data/pureport/client -c /data/models/swagger/config.json --invoker-package 'pureport'
+docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli:2.4.9 generate -i /data/models/swagger/kato_swagger.json -l go -o /data/pureport/client -c /data/models/swagger/config.json --invoker-package 'pureport'
