22da0b751d6e7604ccf0a9efb3e7a059730bf45b
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-11-13 15:42:55 -0500

    Add updated generator for OpenAPI

--- models/swagger/generate.go
+++ models/swagger/generate.go
@@ -1,5 +1,5 @@
 // Package swagger Generator based on the swagger API definition
 package swagger
 
-//go:generate docker run --rm -v $PWD:/data swaggerapi/swagger-codegen-cli:2.4.5 generate -i /data/models/swagger/pureport_swagger.json -l go -o /data/pureport/client -c /data/models/swagger/config.json --invoker-package pureport\client
+//go:generate docker run --rm -v $PWD:/data openapitools/openapi-generator-cli:v4.2.0 generate -i /data/models/swagger/pureport_swagger.json -g go -o /data/pureport/client -c /data/models/swagger/config.json --enable-post-process-file -p packageName='pureport' -p packageVersion=1.2.1
 //go:generate gofmt -s -w ../../pureport/client
